7530ec268fdf5cb3b70fedd11d0227864857057e
its-ayasha <64803283+its-ayasha@users.noreply.github.com>
2020-10-01 00:36:02 +0530

    Update GameLiftGameInstance.h

--- Source/GameLift/Classes/GameLiftGameInstance.h
+++ Source/GameLift/Classes/GameLiftGameInstance.h
@@ -1,21 +1,16 @@
 //Copyright 2016 davevillz, https://github.com/davevill
 
-
-
 #pragma once
 
 #include "Engine/GameInstance.h"
 #include "GameLiftGameInstance.generated.h"
 
 
-
-
-/** This class is optional, the ideal way is to create, initialize and shutdown the GameLiftManager object within your own game instance */
+/** This class is optional, the ideal way to do this is to create, initialize and shutdown the GameLiftManager object within your own game instance */
 UCLASS()
 class GAMELIFT_API UGameLiftGameInstance : public UGameInstance
 {
 	GENERATED_UCLASS_BODY()
-
 	UPROPERTY()
 	class UGameLiftManager* GameLiftManager;
 
